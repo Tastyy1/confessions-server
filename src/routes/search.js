5d5090b185f54b1a317c2d758142b3b45c6a282c
Tasty <yusufkarakoc06@hotmail.com>
2023-11-20 21:10:45 +0300

    Update search.js

--- src/routes/search.js
+++ src/routes/search.js
@@ -4,11 +4,6 @@ import Post from "../models/Post.js";
 
 const router = Router();
 
-// Veritabanında metin indeksi oluşturmak için bu adımı gerçekleştirebilirsiniz
-// Bu adım sadece bir kere yapılmalıdır
-// Örneğin, terminalde "mongo" komutunu kullanarak MongoDB'ye bağlanın ve aşağıdaki komutu girin:
-// db.posts.createIndex({ name: "text", body: "text" });
-
 router.get("/", searchLimiter, async (req, res) => {
   const { q, sort } = req.query;
 
@@ -21,46 +16,50 @@ router.get("/", searchLimiter, async (req, res) => {
     });
   }
 
-  try {
-    const result = await Post.aggregate([
-      {
-        $match: {
-          $and: [
-            {
-              $text: {
-                $search: q,
-                $caseSensitive: false,
-              },
-            },
-            { "meta.isDeleted": false },
-          ],
-        },
-      },
-      {
-        $project: {
-          name: 1,
-          body: 1,
-          count: 1,
-          engagement: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          score: {
-            $meta: "textScore",
-          },
-        },
-      },
-      {
-        $limit: 30,
-      },
-      sort === "top"
-        ? {
-            $sort: {
-              engagement: -1,
-            },
-          }
-        : {}, // Eğer sıralama isteği "top" ise sıralamayı ekleyin, değilse eklemeyin
-    ]);
+  // Veritabanında "name" ve "body" alanlarında metin indeksi oluşturulmuş olmalı
+  const textQuery = {
+    $text: {
+      $search: q,
+      $caseSensitive: false,
+    },
+  };
 
+  const matchQuery = {
+    $match: {
+      $and: [textQuery, { "meta.isDeleted": false }],
+    },
+  };
+
+  const projection = {
+    $project: {
+      name: 1,
+      body: 1,
+      count: 1,
+      engagement: 1,
+      createdAt: 1,
+      updatedAt: 1,
+      score: {
+        $meta: "textScore",
+      },
+    },
+  };
+
+  const limitStage = {
+    $limit: 30,
+  };
+
+  const pipeline = [matchQuery, projection, limitStage];
+
+  if (sort === "top") {
+    pipeline.splice(1, 0, {
+      $sort: {
+        engagement: -1,
+      },
+    });
+  }
+
+  try {
+    const result = await Post.aggregate(pipeline);
     res.status(200).json({
       status: "success",
       message: "Search results",
